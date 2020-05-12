02884fbb14241cef0a70f5b864d00470d7a7911d
Son Lam <lamns@lumi.biz>
2020-05-12 22:21:30 +0700

    add morgan debug

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
+const morgan = require('morgan');
 
 
 const authenRouter = require('./routes/authenRouter');
@@ -20,6 +21,8 @@ const app = express();
 // Allow Cross-Origin requests
 app.use(cors());
 
+app.use(morgan('combined'));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -33,7 +36,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({
-    limit: '15kb'
+    limit: '50mb'
 }));
 
 // Data sanitization against Nosql query injection
